f6bb8f53066a0b79d3ac053f9d61839d8ffe26d8
Mahesh Chavhan <chavhanmahesh1997@gmail.com>
2024-05-23 23:35:20 +0530

    second msg

--- src/com/git/sample/Greet.java
+++ src/com/git/sample/Greet.java
@@ -3,7 +3,7 @@ package com.git.sample;
 public class Greet {
 public static void main(String args[]) {
 	
-	System.out.println("Hello . . . !");
+	System.out.println("Hello . . . !\nHow are You ?");
 	
 }
 }
